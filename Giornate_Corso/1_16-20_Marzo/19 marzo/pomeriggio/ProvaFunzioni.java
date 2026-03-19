ddbdeb84fbdd8b5ef2263b3cdefe56e91bc4bb67
Gabriele <gabriele.campomizzi@gmail.com>
2026-03-19 12:50:53 +0100

    12:50_MAR19

--- Giornate_Corso/1_16-20_Marzo/19 marzo/pomeriggio/ProvaFunzioni.java
+++ Giornate_Corso/1_16-20_Marzo/19 marzo/pomeriggio/ProvaFunzioni.java
@@ -3,8 +3,12 @@ public class ProvaFunzioni {
     System.out.println("Ciao, mondo!");
     }
 
+    static int somma(int a, int b) {
+        return a + b;
+    }
+
     public static void main(String[] args){
         saluta(); //chiamata del metodo
+        System.out.println(somma(10, 20)); //output: 30
     }
-    
 }
